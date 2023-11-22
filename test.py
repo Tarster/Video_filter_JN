dc4c8e8579f3a195e4c94b942c5ece963038ba15
Tarster <sharmatarun537@gmail.com>
2023-11-22 23:37:14 +0000

    face and test

--- test.py
+++ test.py
@@ -1,18 +1,59 @@
-import numpy as np
+# import mediapipe as mp
+# from mediapipe.tasks import python
+# from mediapipe.tasks.python import vision
+
+# model_path = 'face_landmarker.task'
 
 import cv2 as cv
-import os
+import mediapipe as mp
+video = cv.VideoCapture(0)
 
-directory = r'Video_filter_JN\background_images' 
-# iterate over files in
-# that directory
-count = 0
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # checking if it is a file
-    test_file = cv.imread(f)
-    # print(test_file)
-    Gaussian = cv.GaussianBlur(test_file,(31,31),0)
-    resized = cv.resize(Gaussian, (960,540))
-    cv.imwrite(directory + "/result/" + "background_{}.jpg".format(count), resized)
-    count += 1
+mp_drawing = mp.solutions.drawing_utils
+mp_face_mesh = mp.solutions.face_mesh
+
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+connection_drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+tm = cv.TickMeter()
+with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5) as face_mesh:
+    while True:
+        ret, image = video.read()
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        tm.start()
+        results = face_mesh.process(image)
+        image.flags.writeable = True
+        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(image, face_landmarks, mp_face_mesh.FACEMESH_CONTOURS,
+                                         landmark_drawing_spec=drawing_spec) #, connection_drawing_spec=drawing_spec)
+        tm.stop()
+        cv.putText(image, 'FPS: {:.2f}'.format(tm.getFPS()), (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+        cv.imshow('Video', image)
+        k = cv.waitKey(1)
+        if k == ord('q'):
+            break
+        tm.reset()
+    video.release()
+    cv.destroyAllWindows()
+
+
+
+# BaseOptions = mp.tasks.BaseOptions
+# FaceLandmarker = mp.tasks.vision.FaceLandmarker
+# FaceLandmarkerOptions = mp.tasks.vision.FaceLandmarkerOptions
+# FaceLandmarkerResult = mp.tasks.vision.FaceLandmarkerResult
+# VisionRunningMode = mp.tasks.vision.RunningMode
+
+# # Create a face landmarker instance with the live stream mode:
+# def print_result(result: FaceLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+#     print('face landmarker result: {}'.format(result))
+
+# options = FaceLandmarkerOptions(
+#     base_options=BaseOptions(model_asset_path=model_path),
+#     running_mode=VisionRunningMode.LIVE_STREAM,
+#     result_callback=print_result)
+
+# with FaceLandmarker.create_from_options(options) as landmarker:
+#     mp_image = mp.Image(image_format=mp.ImageFormat.RGB, data=numpy_frame_from_opencv)
+#     landmarker.detect_async(mp_image, frame_timestamp_ms)
